6371b3ac74f09ab86d51e60de691b50576a6c15e
pitatumisang <pitatumisang@gmail.com>
2022-01-18 21:06:18 +0200

    Error handling middleware update

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -4,6 +4,23 @@ const errorHandler = (err, req, res, next) => {
   let message = err.message || 'Something went wrong, try again';
   let statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
 
+  if (err.code === 11000) {
+    message = 'Email already exists';
+    statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.name === 'CastError') {
+    message = `Todo with id: ${err.value} not found`;
+    statusCode = StatusCodes.NOT_FOUND;
+  }
+
+  if (err.name === 'ValidationError') {
+    message = Object.values(err.errors)
+      .map((value) => value.message)
+      .toString();
+    statusCode = StatusCodes.BAD_REQUEST;
+  }
+
   res.status(statusCode).json({ success: false, msg: message });
 };
 
